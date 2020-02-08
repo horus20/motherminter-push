1c69bd91486b79920651eb3ed92efc8070a73150
Horus <horus20@mail.ru>
2020-02-08 13:22:54 +0700

    fix fiat

--- www/pages/index.vue
+++ www/pages/index.vue
@@ -432,6 +432,9 @@
       balanceSum() {
         const fiatVal = getFiatByLocale(this.currentLang)
         const fiatSymbol = fiatVal ? fiatVal.symbol : ''
+        if (this.currentLang === 'en') {
+          return `${this.balanceSumUSD.toFixed(2)} $`
+        }
         return `${this.balanceSumFiat.toFixed(2)} ${fiatSymbol}`
       },
     },
