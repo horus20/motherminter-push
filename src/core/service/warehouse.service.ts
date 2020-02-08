49c3f30829ef322ee3529f9cf3492c43b891160a
Horus <horus20@mail.ru>
2020-02-08 13:19:01 +0700

    change comission

--- src/core/service/warehouse.service.ts
+++ src/core/service/warehouse.service.ts
@@ -101,7 +101,7 @@ export class WarehouseService {
     let type = TX_TYPE.SEND;
     let feeSymbol = symbol;
     let isValid = true;
-    const feeBip = 0.011;
+    const feeBip = 0.01;
 
     if (amount === '') {
       // send all balances
