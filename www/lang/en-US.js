09215609218bf5d5c6418e07ef240ae00b6e8e16
Mother Minter <mikhail@zhivayaset.ru>
2020-02-10 09:41:23 +0300

    Update en-US.js

--- www/lang/en-US.js
+++ www/lang/en-US.js
@@ -95,7 +95,7 @@ export default {
     charityFund: 'Charity|fund',
     spendSection: 'or spend them on',
     mobileService: 'Top-up|mobile',
-    games: 'Games',
+    games: 'Fun&|Games',
     foodDelivery: 'NUT|loyalty',
     haveWallet: 'Does the person you send coins have a wallet address',
     createNewPush: 'Create a unique link-wallet and transfer your balance to it',
